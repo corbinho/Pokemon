55833f8bc6cb8b279fa768920fec9c8fad83eddc
corbin.felsheim@gmail.com <corbinfelsheim@Corbins-MacBook-Pro.local>
2019-04-30 17:01:28 -0700

    commiting react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
 // import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import DraftMinion from "./components/draftMinion/draftMinion";
 
 class App extends Component {
   render() {
     return (
-      <DraftMinion></DraftMinion>
+      <Router>
+      <div>
+       
+        <Switch>
+          <Route exact path="/api/minion" component={DraftMinion} />
+          
+        </Switch>
+      </div>
+    </Router>
     );
   }
 }
