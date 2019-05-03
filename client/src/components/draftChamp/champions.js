4beb0a3caa94c474065ca91524b0f68b7ccce780
corbin.felsheim@gmail.com <corbinfelsheim@Corbins-MacBook-Pro.local>
2019-05-03 14:37:14 -0700

    adding champion draft

---
+++ client/src/components/draftChamp/champions.js
@@ -0,0 +1,51 @@
+var championList = [
+    {
+      "id": "0",
+      "name": "Charizard",
+      "type": "../images/fireOrb.png",
+      "Health": 80,
+      "WeakAgainst": "../images/waterOrb.png",
+      "StrongAgainst": "../images/grassOrb.png",
+      "Img": "../images/Charizard.jpg"
+    },
+    {
+      "id": "1",
+      "name": "Blastoise",
+      "type": "../images/waterOrb.png",
+      "Health": 80,
+      "WeakAgainst": "../images/electricOrb.png",
+      "StrongAgainst": "../images/darkOrb.png",
+      "Img": "../images/Blastoise.jpg"
+    },
+    {
+      "id": "2",
+      "name": "Venusaur",
+      "type": "../images/grassOrb.png",
+      "Health": 80,
+      "WeakAgainst": "../images/fireOrb.png",
+      "StrongAgainst": "../images/waterOrb.png",
+      "Img": "../images/Venasaur.png"
+    },
+    {
+      "id": "3",
+      "name": "Mewtwo",
+      "type": "psychic",
+      "Health": 80,
+      "WeakAgainst": "../images/darkOrb.png",
+      "StrongAgainst": "../images/fightingOrb.png",
+      "Img": "../images/Mewtwo.jpg"
+    },
+    {
+      "id": "4",
+      "name": "Pikachu",
+      "type": "../images/electricOrb.png",
+      "Health": 80,
+      "WeakAgainst": "../images/groundOrb.png",
+      "StrongAgainst": "../images/waterOrb.png",
+      "Img": "../images/pikachu.jpg"
+    }
+  ]
+
+  module.exports = {
+    championList: championList
+}
--- client/src/components/draftChamp/draftChamp.js
+++ client/src/components/draftChamp/draftChamp.js
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
+import championList from "./champions"
+import "./DraftChampion.css"
 
-function draftChamp(props) {
-    return <div>
-        <h3 className="champName">{props.name}</h3>
-        <p className="champHealth">{props.health}</p>
-        <p className="champattack1">{props.attack1}</p>
-        <p className="champattack1cost">{props.attack1cost}</p>
-        <p className="champattack2">{props.attack2}</p>
-        <p className="champattack2cost">{props.attack2cost}</p>
-        <p className="weakness">{props.weakness}</p>
-        <p className="stength">{props.strength}</p>
-        <img className= "champImage" src={props.image}></img>
-    </div>
+class DraftChamp extends Component {
+  state = {
+    champions: championList.championList,
+    player1champion: [],
+    player2champion: []
   }
 
-  export default draftChamp;
+  selectChampion = index => {
+    let selectedChampion = [...this.state.champions]
+    this.setState({
+      player1Champion: [...this.state.player1Champion, selectedChampion[index]]
+    })
+    console.log(this.state.player1Champion)
+  }
+
+  render() {
+    return(
+    this.state.champions.map((champion) => (
+        <div className= "championCard" id= {champion.id} key={champion.id} onClick={() => this.selectChampion(champion.id)}>
+
+        <h3 className="championName">{champion.name || "champion"}</h3>
+        <p className="championHealth">{champion.Health || 2}</p>
+
+        <p className="championCost">{champion.playCost || 6}</p>
+        <img className="championWeakness" src={champion.WeakAgainst} alt="" width="42" height="1"></img>
+        <img className="championStrength" src={champion.StrongAgainst} alt="" width="5" height="1"></img>
+        <img className= "championPortrait" src={champion.Img} alt=""></img>
+      </div> 
+    )
+    )
+    )
+    }
+}
+  export default DraftChamp;
