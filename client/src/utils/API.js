bfcd0ca55a8f7c254bdd108a793adf2bad24b54d
Jacob Janak <jakeathotmail@gmail.com>
2019-05-17 02:45:53 -0400

    Jake's changes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,9 +1,21 @@
 import axios from "axios";
+import openSocket from 'socket.io-client';
+
+const socket = openSocket(window.location.origin);
 
 export default {
   // Gets all minions
   getMinions: function() {
     return axios.get("/api/minions");
   },
-  
+
+  // Socket stuff below
+  joinGame: (cb) => {
+    socket.emit('joinGame', { })
+    socket.on('updateGame', cb)
+  },
+  draftChampion: (champion, cb) => {
+    socket.emit('draftChampion', champion)
+    // socket.on
+  }
 };
