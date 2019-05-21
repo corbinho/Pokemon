87008752fda0321becaa08ee5de042bffdf8e8f9
corbin.felsheim@gmail.com <corbinfelsheim@Corbins-MacBook-Pro.local>
2019-05-20 18:46:14 -0700

    commitingv3

--- routes/api/index.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const minionRoutes = require("./minion");
-
-// minion routes
-router.use("/minions", minionRoutes);
-
-module.exports = router;
--- routes/api/minion.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const minionController = require("../../controllers/minionController");
-
-// Matches with "/api/minions?"
-router.route("/")
-  .get(minionController.findAll)
-  
-module.exports = router;
